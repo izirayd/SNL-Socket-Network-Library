1fc52c751b64f83c716715ad008017c2506c4a13
izirayd <izirayd@mail.ru>
2017-03-05 10:43:13 +0300

    Add example hellp

---
+++ example/helloworld/hello.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "../../Socket.hpp"
+
+using namespace std::chrono_literals;
+std::chrono::time_point<std::chrono::steady_clock> start;
+std::chrono::time_point<std::chrono::steady_clock> end;
+
+void ReadPacket(std::socket_t Socket, std::base_socket::byte_t *Buffer)
+{
+	end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> elapsed = end - start;
+	printf("\nPacket[%d - %lf ms]: %s", Socket, elapsed.count(), Buffer);	
+}
+
+int main()
+{
+	std::base_socket::init_socket();
+
+	std::server server;
+	std::client client;
+
+	server["read"] = ReadPacket;
+	client["read"] = ReadPacket;
+
+	server.Create("127.0.0.1", 500, std::arch_server_t::tcp_thread);
+	server.Run(std::type_blocked_t::non_block);
+
+	client.Connect("127.0.0.1", 500, std::arch_server_t::tcp_thread);
+	client.Run(std::type_blocked_t::non_block);
+
+	for (;;) {
+		start = std::chrono::high_resolution_clock::now();
+		client.Send("Hello, World!\0", 15);
+
+		std::this_thread::sleep_for(1ms);
+	}
+
+	return 0;
+}
+
